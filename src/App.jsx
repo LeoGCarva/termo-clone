eff4c4481ff42d60c7aa443f9f79fea0371739da
Leonardo <leu1.g.carvalho@gmail.com>
2025-04-05 20:59:49 -0300

    Style Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,56 @@
+import "./App.css";
 import { useState } from "react";
 
-const palavrasPossiveis = ['legal', 'tenis', 'livro', 'teste', 'pinto', 'bolas', 'casal', 'carro', 'sigla', 'nobre',
-  'fazer', 'termo', 'assim', 'fosse', 'moral', 'sobre', 'poder', 'senso', 'justo', 'honra', 'muito', 'sonho', 'ideia'
-]
-const palavraDoDia = palavrasPossiveis[Math.round(Math.random() * (palavrasPossiveis.length + 1))];
+const palavrasPossiveis = [
+  "legal",
+  "tenis",
+  "livro",
+  "teste",
+  "pinto",
+  "bolas",
+  "casal",
+  "carro",
+  "sigla",
+  "nobre",
+  "fazer",
+  "termo",
+  "assim",
+  "fosse",
+  "moral",
+  "sobre",
+  "poder",
+  "senso",
+  "justo",
+  "honra",
+  "muito",
+  "sonho",
+  "ideia",
+];
+const palavraDoDia =
+  palavrasPossiveis[Math.round(Math.random() * (palavrasPossiveis.length + 1))];
 
 export function App() {
   const [tentativas, setTentativas] = useState([]);
-  const [entradaAtual, setEntradaAtual] = useState('');
+  const [entradaAtual, setEntradaAtual] = useState("");
   const [resultado, setResultado] = useState([]);
   const [fimDeJogo, setFimDeJogo] = useState(false);
 
   function verificarTentativa(tentativa) {
-    const res = Array(5).fill('cinza');
+    const res = Array(5).fill("var(--pearl)");
     const letrasUsadas = [];
 
-    for(let i = 0; i < 5; i++){
-      if(tentativa[i] === palavraDoDia[i]){
-        res[i] = 'verde';
+    for (let i = 0; i < 5; i++) {
+      if (tentativa[i] === palavraDoDia[i]) {
+        res[i] = "var(--green)";
         letrasUsadas.push(i);
       }
     }
 
-    for(let i = 0; i < 5; i++) {
-      if(res[i] === 'cinza') {
-        for(let j = 0; j < 5; j++) {
-          if(!letrasUsadas.includes(j) && tentativa[i] === palavraDoDia[j]) {
-            res[i] = 'amarelo';
+    for (let i = 0; i < 5; i++) {
+      if (res[i] === "var(--pearl)") {
+        for (let j = 0; j < 5; j++) {
+          if (!letrasUsadas.includes(j) && tentativa[i] === palavraDoDia[j]) {
+            res[i] = "var(--yellow)";
             letrasUsadas.push(j);
             break;
           }
@@ -35,50 +59,72 @@ export function App() {
     }
 
     return res;
-  };
-
-function handleSubmit() {
-  if(entradaAtual.length !== 5) {
-    alert('Palavra inválida!');
-    return;
   }
 
-  const novaTentativas = [...tentativas, entradaAtual];
-  const novoResultado = [...resultado, verificarTentativa(entradaAtual)];
+  function handleSubmit() {
+    if (entradaAtual.length !== 5) {
+      alert("Palavra inválida!");
+      return;
+    }
 
-  setTentativas(novaTentativas);
-  setResultado(novoResultado);
-  setEntradaAtual('');
+    const novaTentativas = [...tentativas, entradaAtual];
+    const novoResultado = [...resultado, verificarTentativa(entradaAtual)];
 
-  if (entradaAtual === palavraDoDia || novaTentativas.length === 6) {
-    setFimDeJogo(true);
+    setTentativas(novaTentativas);
+    setResultado(novoResultado);
+    setEntradaAtual("");
+
+    if (entradaAtual === palavraDoDia || novaTentativas.length === 6) {
+      setFimDeJogo(true);
+    }
   }
-}
+
+  // function letraContainer(letter, color) {
+  //   return (
+  //     <>
+  //       <span key={letter} style={{ marginRight: 5 }}>
+  //         {letter.toUpperCase()} ({color})
+  //       </span>
+  //     </>
+  //   );
+  // }
+  console.log(tentativas);
+  console.log(resultado)
 
   return (
-    <div>
-      {!fimDeJogo ? (
-        <div>
-          <input
-            maxLength={5}
-            value={entradaAtual}
-            onChange={(e) => setEntradaAtual(e.target.value.toLowerCase())}
-          />
-          <button onClick={handleSubmit}>Enviar</button>
-        </div>
-      ) : (
-        <p>Fim de jogo! A palavra era: {palavraDoDia}</p>
-      )}
+    <>
+      <div className="divContainer">
+        <h2>Palavra</h2>
+        {!fimDeJogo ? (
+          <div className="inputBtnContainer">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit();
+              }}
+            >
+              <input
+                className="inputTermo"
+                maxLength={5}
+                value={entradaAtual}
+                onChange={(e) => setEntradaAtual(e.target.value.toLowerCase())}
+              />
+            </form>
+          </div>
+        ) : (
+          <p>Fim de jogo! A palavra era: {palavraDoDia}</p>
+        )}
 
-      {tentativas.map((tentativa, i) => (
-        <div key={i}>
-          {tentativa.split('').map((letra, j) => (
-            <span key={j} style={{ marginRight: 5 }}>
-              {letra.toUpperCase()} ({resultado[i][j]})
-            </span>
-          ))}
-        </div>
-      ))}
-    </div>
+        {tentativas.map((tentativa, i) => (
+          <div key={i} className="tryContainer">
+            {tentativa.split("").map((letra, j) => (
+              <span className="letterSpan" key={j} style={{ marginRight: 5, backgroundColor: resultado[i][j] }}>
+                {letra.toUpperCase()}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
